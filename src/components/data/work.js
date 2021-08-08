2e644cd93e479687b007e1c8fe53b5d4b59b906f
layan2k <lesliedeeshumba@gmail.com>
2021-08-08 03:40:11 +0200

    fix1

--- src/components/data/work.js
+++ src/components/data/work.js
@@ -4,7 +4,7 @@ export const WorkData= [
         Role:"Web Developer",
         dateJoining: "01-05-2021",
         dateEnd:"Present",
-        companyLogo:"https://static.middlemanafrica.com/images/logo.png",
+        companyLogo: require("../../assets/mmd.png").default,
         work:"Developing Websites for Clients All Over Africa."
 
     },
@@ -13,7 +13,7 @@ export const WorkData= [
         Role:"Student",
         dateJoining: "01-06-2021",
         dateEnd:"05-06-2022",
-        companyLogo:"https://lh3.googleusercontent.com/oVJxT1yn7vwaEM8t9A5MGL6emG0j-_uqHa5H8ikWLvl6Ka-nVmUJZblqWDqPiY-S6itPLnZNgcc8rviK8AVT65l_a3zHiyctwy8=s0",
+        companyLogo: require("../../assets/alx.png").default,
         work:"Software Engineering Certificate(1-year). The Program is done under the partnership of ALX and Holberton School",
 
     }
