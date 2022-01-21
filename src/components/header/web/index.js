c84e872524bee7ab1d2dc5b584a234cca536c9c4
Leslie D Shumba <84179968+layan2k@users.noreply.github.com>
2022-01-21 18:59:03 +0200

    Restore

--- src/components/header/web/index.js
+++ src/components/header/web/index.js
@@ -6,22 +6,22 @@ function Web() {
         <div className="web">
             <div className="web-option">
                 <a href="#projects" >
-                <i class="fi fi-rr-edit-alt"></i>Projects
+                <i class="fi fi-rr-edit-alt option-icons"></i>Projects
                 </a>
             </div>
             <div className="web-option">
                 <a href="#skills" >
-                <i class="fi fi-rr-laptop"></i>Skills
+                <i class="fi fi-rr-laptop option-icons"></i>Skills
                 </a>
             </div>
             <div className="web-option">
                 <a href="#resume" >
-                <i class="fi fi-rr-form"></i>Resume
+                <i class="fi fi-rr-form option-icons"></i>Resume
                 </a>
             </div>
             <div className="web-option">
                 <a href="#contact" >
-                <i class="fi fi-rr-portrait"></i>Contact
+                <i class="fi fi-rr-portrait option-icons"></i>Contact
                 </a>
             </div>
         </div>
